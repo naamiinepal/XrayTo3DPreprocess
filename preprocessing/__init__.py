57992bc7266f3cb0f747389688f0691eeec2384b
msrepo <maheshshakya.01@gmail.com>
2022-12-20 09:56:34 +0545

    driver

---
+++ preprocessing/__init__.py
@@ -0,0 +1,1 @@
+from .utils import load_centroids, read_image, ImagePixelType, extract_around_centroid, write_image
---
+++ preprocessing/extract_regions.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import argparse
+
+from argparse import ArgumentParser
+
+from preprocessing import load_centroids,read_image, ImagePixelType, extract_around_centroid, write_image
+
+parser = ArgumentParser(description='extract region whose centroid are given')
+parser.add_argument('--ct',required=True)
+parser.add_argument('--seg',required=True)
+parser.add_argument('--centroids',required=True)
+parser.add_argument('-o',required=True)
+
+args = parser.parse_args()
+
+print(args)
+
+
+ctd_list = load_centroids(args.centroids)
+img = read_image(args.ct,pixeltype=ImagePixelType.ImageType)
+seg = read_image(args.seg,pixeltype=ImagePixelType.SegmentationType)
+
+print(ctd_list)
+vb_id,*ctds = ctd_list[5]
+print(vb_id,ctds)
+
+roi = extract_around_centroid(img, (100,100,100), ctds,-1024)
+write_image(roi,args.o)
