9035824b60a1938eab1566d93795c7ee4405ec28
msrepo <maheshshakya.01@gmail.com>
2023-01-04 10:31:47 +0545

    add all_elements_equal

--- preprocessing/tuple_ops.py
+++ preprocessing/tuple_ops.py
@@ -1,4 +1,5 @@
 import math
+from typing import List
 
 
 def add_tuple(a, b):
@@ -28,3 +29,12 @@ def ceil_tuple(a):
 
 def floor_tuple(a):
     return tuple(math.floor(a_i) for a_i in a)
+
+def all_elements_equal(arr:List):
+    '''
+    >>> all_elements_equal([1.0,1.0,1.0])
+    True
+    >>> all_elements_equal([1.0,1.5,1.0])
+    False
+    '''
+    return arr.count(arr[0]) == len(arr)
