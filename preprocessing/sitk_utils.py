15db0a517e733fe574af5d2b18b650a9fb0b4e6a
msrepo <maheshshakya.01@gmail.com>
2023-01-04 10:33:57 +0545

    reorientation and resampling

--- preprocessing/sitk_utils.py
+++ preprocessing/sitk_utils.py
@@ -1,6 +1,7 @@
 import SimpleITK as sitk
 from .enumutils import ProjectionType
-from .metadata_utils import get_orientation_code
+from .tuple_ops import all_elements_equal
+from .metadata_utils import get_orientation_code_itk
 
 def change_label(img: sitk.Image, mapping_dict) -> sitk.Image:
     """use SimplITK AggregateLabelMapFilter to merge all segmentation labels to first label. This is used to obtain the bounding box of all the labels """
@@ -12,8 +13,48 @@ def keep_only_label(segmentation:sitk.Image, label_id) -> sitk.Image:
     """If the segmentation contains more than one labels, keep only label_id"""
     return sitk.Threshold(segmentation, label_id, label_id, 0)
 
+def get_interpolator(interpolator: str):
+    """Utility function to converte string representation of interpolator to sitk.sitkInterpolator type"""
+    if interpolator == 'nearest':
+        return sitk.sitkNearestNeighbor
+    if interpolator == 'linear':
+        return sitk.sitkLinear
+
+def make_isotropic(img: sitk.Image, spacing=None, interpolator='linear'):
+    '''
+    Resample `img` so that the voxel is isotropic with given physical spacing
+    The image volume is shrunk or expanded as necessary to represent the same physical space. 
+    Use sitk.sitkNearestNeighbour while resampling Label images,
+    when spacing is not supplied by the user, the highest resolution axis spacing is used
+    '''
+    # keep the same physical space, size may shrink or expand
+
+    if spacing is None:
+        spacing = min(list(img.GetSpacing()))
+
+    original_spacing = list(img.GetSpacing())
+    if all_elements_equal(original_spacing) and original_spacing[0] == spacing:
+        return img
+
+    resampler = sitk.ResampleImageFilter()
+    new_size = [round(old_size * old_spacing / spacing)
+                for old_size, old_spacing in zip(img.GetSize(), img.GetSpacing())]
+    output_spacing = [spacing]*len(img.GetSpacing())
+
+    resampler.SetOutputSpacing(output_spacing)
+    resampler.SetSize(new_size)
+    resampler.SetOutputDirection(img.GetDirection())
+    resampler.SetOutputOrigin(img.GetOrigin())
+    resampler.SetTransform(sitk.Transform())
+    resampler.SetInterpolator(get_interpolator(interpolator))
+    resampler.SetDefaultPixelValue(
+        img.GetPixelIDValue())
+
+    return resampler.Execute(img)
+
 def simulate_projection(segmentation:sitk.Image,projectiontype:ProjectionType) -> sitk.Image:
-    orientation = get_orientation_code(segmentation)
+    segmentation = make_isotropic(segmentation,1.0,"nearest")
+    orientation = get_orientation_code_itk(segmentation)
     orientation = list(orientation)
 
     if projectiontype == ProjectionType.ap:
@@ -55,3 +96,70 @@ def rotate_about_image_center(img: sitk.Image, rx,ry,rz):
     transform.SetCenter(isocenter)
 
     return sitk.Resample(img,transform=transform)
+
+
+def reorient_to(img, axcodes_to='PIR', verb=False):
+    """Reorients the Image from its original orientation to another specified orientation
+    
+    Parameters:
+    ----------
+    img: SimpleITK image
+    axcodes_to: a string of 3 characters specifying the desired orientation
+    
+    Returns:
+    ----------
+    new_img: The reoriented SimpleITK image 
+    
+    """
+    new_img = sitk.DICOMOrient(img,axcodes_to)
+
+    if verb:
+        print("[*] Image reoriented from", get_orientation_code_itk(img), "to", axcodes_to)
+    return new_img
+
+def reorient_centroids_to(ctd_list, target_axcodes,image_shape, decimals=1, verb=False):
+    # adapted from github.com/anjany/verse
+    """reorient centroids to image orientation
+    
+    Parameters:
+    ----------
+    ctd_list: list of centroids [ the first element of the list should be axcode ]
+    target_axcodes: for example: 'PIR'
+    image_shape: (sx,sy,sz) This is required when inverting axis 
+    decimals: rounding decimal digits
+    
+    Returns:
+    ----------
+    out_list: reoriented list of centroids 
+    
+    """
+    import numpy as np
+    import nibabel.orientations as nio
+
+    if isinstance(target_axcodes,str):
+        target_axcodes = list(target_axcodes) #'PIR' -> ['P', 'I', 'R']
+
+    ctd_arr = np.transpose(np.asarray(ctd_list[1:]))
+    if len(ctd_arr) == 0:
+        print("[#] No centroids present") 
+        return ctd_list
+    v_list = ctd_arr[0].astype(int).tolist()  # vertebral labels
+    ctd_arr = ctd_arr[1:]
+    ornt_fr = nio.axcodes2ornt(ctd_list[0])  # original centroid orientation
+    ornt_to = nio.axcodes2ornt(target_axcodes)
+    trans = nio.ornt_transform(ornt_fr, ornt_to).astype(int)
+    perm = trans[:, 0].tolist()
+    shp = np.asarray(image_shape)
+    ctd_arr[perm] = ctd_arr.copy()
+    for ax in trans:
+        if ax[1] == -1:
+            size = shp[ax[0]]
+            ctd_arr[ax[0]] = np.around(size - ctd_arr[ax[0]], decimals)
+    out_list = [target_axcodes]
+    ctd_list = np.transpose(ctd_arr).tolist()
+    for v, ctd in zip(v_list, ctd_list):
+        out_list.append([v] + ctd)
+    if verb:
+        print("[*] Centroids reoriented from", nio.ornt2axcodes(ornt_fr), "to", target_axcodes)
+    return out_list
+
