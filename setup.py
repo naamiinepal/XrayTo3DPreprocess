b825862c8fbffbaff49ac59c16378241199b04b9
msrepo <maheshshakya.01@gmail.com>
2023-01-06 21:54:22 +0545

    add install setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(name='xrayto3d_preprocess',
+      version="0.1.0",
+      description="Preprocessing utils and scripts for Dataset creation for X-ray to 3D Shape Reconstruction",
+      url="https://github.com/naamiinepal/XrayTo3DPreprocess",
+      author="Mahesh Shakya",
+      author_email="mahesh.shakya@naamii.org.np",
+      packages=find_packages(),
+      keywords="Python utilities simpleitk biomedical",
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Science/Research',
+                   'Topic :: Scientific/Engineering :: Medical Science Apps.',
+
+                   # Pick your license as you wish (should match "license" above)
+                   'License :: OSI Approved :: MIT License',
+
+                   # Specify the Python versions you support here. In particular, ensure
+                   # that you indicate whether you support Python 2, Python 3 or both.
+                   'Programming Language :: Python :: 3.6'
+                   ])
