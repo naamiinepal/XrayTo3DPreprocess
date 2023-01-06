652e361f9e2893a93e77b4f3df521569846fb7dc
msrepo <maheshshakya.01@gmail.com>
2023-01-06 14:47:40 +0545

    separate get_nifti_stem and get_stem

--- preprocessing/pathutils.py
+++ preprocessing/pathutils.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 # from torchio/utils.py
 
-def get_stem(path):
+def get_nifti_stem(path):
     """
     '/home/user/image.nii.gz' -> 'image'
     1.3.6.1.4.1.14519.5.2.1.6279.6001.905371958588660410240398317235.nii.gz ->1.3.6.1.4.1.14519.5.2.1.6279.6001.905371958588660410240398317235
@@ -13,7 +13,9 @@ def get_stem(path):
     if isinstance(path, (str, os.PathLike)):
         return _get_stem(path)
 
-
+def get_stem(path):
+    return Path(path).stem
+    
 def get_file_format_suffix(path):
     """
     '/home/user/image.nii.gz' -> 'nii.gz'
@@ -27,7 +29,7 @@ def dest_path(source_path, dest_dir,dest_format=None):
     """
     '/home/user/image.nii.gz' , /home/dest -> /home/dest/image.nii.gz 
     """
-    filename = get_stem(source_path)
+    filename = get_nifti_stem(source_path)
     if dest_format is None:
         dest_format = get_file_format_suffix(source_path)
     return Path(dest_dir)/f'{filename}.{dest_format}'
@@ -38,7 +40,7 @@ def get_verse_subject_id(file_path):
     sub-verse417_split-verse277_ct.nii.gz -> sub-verse417_split-verse277
     sub-verse149_ct.nii.gz -> sub-verse149
     """
-    file_stem = get_stem(file_path)
+    file_stem = get_nifti_stem(file_path)
     file_components = file_stem.split('_')
     if len(file_components) > 1 and 'split' in file_components[1]:
         return '_'.join(file_components[0:2])
