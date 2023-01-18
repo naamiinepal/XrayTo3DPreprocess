6d653cdccad49983b1a7d5f1326217321e4ff797
msrepo <maheshshakya.01@gmail.com>
2023-01-18 11:27:35 +0545

    add write_csv

--- xrayto3d_preprocess/misc.py
+++ xrayto3d_preprocess/misc.py
@@ -1,2 +1,13 @@
+import pandas as pd
+from typing import Dict,List,Optional
 def range_inclusive(start, end):
      return range(start, end+1)
+
+def write_csv(data:Dict[str,List],column_names:Optional[List[str]],file_path):
+    """
+    The dictionary should consist of key:List as key value pairs representing a single row.
+    Optional name of each columns may also be provided
+    """
+    df = pd.DataFrame.from_dict(data, orient='index',
+                       columns=column_names)
+    df.to_csv(file_path)
